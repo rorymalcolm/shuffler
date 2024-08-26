7b9417f6d46795d80d0b4456307ab95e86ed7ef9
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2024-08-26 14:43:36 +0100

    lattice tests

---
+++ pkg/lattice/lattice_test.go
@@ -0,0 +1,64 @@
+package lattice_test
+
+import (
+	"testing"
+
+	"github.com/rorymalcolm/shuffler/v2/pkg/lattice"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSingleCellLattice(t *testing.T) {
+	// Create a lattice with a single cell and string endpoints
+	lat := lattice.NewLattice[string]([]string{"cell"})
+
+	// Add endpoints
+	err := lat.AddEndpointsForSector([]string{"cell"}, []string{"A"})
+	assert.NoError(t, err)
+
+	err = lat.AddEndpointsForSector([]string{"cell"}, []string{"B", "C", "D"})
+	assert.NoError(t, err)
+
+	// Check that all endpoints are in with correct ordering
+	allEndpoints := lat.GetAllEndpoints()
+	expectedEndpoints := []string{"A", "B", "C", "D"}
+
+	assert.ElementsMatch(t, expectedEndpoints, allEndpoints)
+}
+
+func TestOneDimensionalLattice(t *testing.T) {
+	// Create a one-dimensional lattice
+	lat := lattice.NewLattice[string]([]string{"AZ"})
+
+	// Add endpoints to different sectors
+	err := lat.AddEndpointsForSector([]string{"us-east-1a"}, []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"})
+	assert.NoError(t, err)
+
+	err = lat.AddEndpointsForSector([]string{"us-east-1b"}, []string{"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"})
+	assert.NoError(t, err)
+
+	// Validate total number of endpoints
+	allEndpoints := lat.GetAllEndpoints()
+	assert.Equal(t, 20, len(allEndpoints))
+}
+
+func TestTwoDimensionalLattice(t *testing.T) {
+	// Create a two-dimensional lattice
+	lat := lattice.NewLattice[string]([]string{"AZ", "Version"})
+
+	// Add endpoints to various sectors
+	err := lat.AddEndpointsForSector([]string{"us-east-1a", "1"}, []string{"A", "B", "C", "D", "E"})
+	assert.NoError(t, err)
+
+	err = lat.AddEndpointsForSector([]string{"us-east-1a", "2"}, []string{"F", "G", "H", "I", "J"})
+	assert.NoError(t, err)
+
+	err = lat.AddEndpointsForSector([]string{"us-east-1b", "1"}, []string{"K", "L", "M", "N", "O"})
+	assert.NoError(t, err)
+
+	err = lat.AddEndpointsForSector([]string{"us-east-1b", "2"}, []string{"P", "Q", "R", "S", "T"})
+	assert.NoError(t, err)
+
+	// Validate total number of endpoints
+	allEndpoints := lat.GetAllEndpoints()
+	assert.Equal(t, 20, len(allEndpoints))
+}
